d4d0f62b6598fed4bc5573f33acf88a65ba97bfe
scx567888 <scx567888@outlook.com>
2025-07-25 14:03:45 +0800

    dev

--- src/main/java/cool/scx/object/mapping/mapper/time/DayOfWeekNodeMapper.java
+++ src/main/java/cool/scx/object/mapping/mapper/time/DayOfWeekNodeMapper.java
@@ -9,23 +9,17 @@ import cool.scx.object.node.Node;
 import cool.scx.object.node.ValueNode;
 
 import java.time.DateTimeException;
-import java.time.Month;
+import java.time.DayOfWeek;
 
-/// MonthNodeMapper
-///
-/// @author scx567888
-/// @version 0.0.1
-public final class MonthNodeMapper implements NodeMapper<Month> {
+public final class DayOfWeekNodeMapper implements NodeMapper<DayOfWeek> {
 
     @Override
-    public Node toNode(Month value, ToNodeContext context) throws NodeMappingException {
-        // 这里我们不以普通的枚举转换成字符串来看待, 而是转换成数字,
-        // 因为 枚举值 字符串 JANUARY, FEBRUARY 等不便于统一, 同时也有大小写的问题
+    public Node toNode(DayOfWeek value, ToNodeContext context) throws NodeMappingException {
         return new IntNode(value.getValue());
     }
 
     @Override
-    public Month fromNode(Node node, FromNodeContext context) throws NodeMappingException {
+    public DayOfWeek fromNode(Node node, FromNodeContext context) throws NodeMappingException {
         //1, 处理 null
         if (node.isNull()) {
             return null;
@@ -34,13 +28,13 @@ public final class MonthNodeMapper implements NodeMapper<Month> {
         // 先使用数字解析
         if (node instanceof ValueNode valueNode) {
             try {
-                return Month.of(valueNode.asInt());
+                return DayOfWeek.of(valueNode.asInt());
             } catch (DateTimeException e) {
                 throw new NodeMappingException(e);
             } catch (NumberFormatException e) {
                 // 无法转换为数字 这里在尝试使用 字符串解析
                 try {
-                    return Month.valueOf(valueNode.asText().toUpperCase());
+                    return DayOfWeek.valueOf(valueNode.asText().toUpperCase());
                 } catch (IllegalArgumentException ex) {
                     throw new NodeMappingException(ex);
                 }
