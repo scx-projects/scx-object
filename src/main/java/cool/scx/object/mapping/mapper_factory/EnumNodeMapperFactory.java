8813dc37fbb2c8e9c6d1bf67a0259dd20f2c8207
scx567888 <scx567888@outlook.com>
2025-07-25 15:45:03 +0800

    dev

---
+++ src/main/java/cool/scx/object/mapping/mapper_factory/EnumNodeMapperFactory.java
@@ -0,0 +1,23 @@
+package cool.scx.object.mapping.mapper_factory;
+
+import cool.scx.object.mapping.NodeMapper;
+import cool.scx.object.mapping.NodeMapperFactory;
+import cool.scx.object.mapping.NodeMapperSelector;
+import cool.scx.object.mapping.mapper.EnumNodeMapper;
+import cool.scx.reflect.ClassInfo;
+import cool.scx.reflect.ClassKind;
+import cool.scx.reflect.TypeInfo;
+
+public class EnumNodeMapperFactory implements NodeMapperFactory {
+
+    @Override
+    public NodeMapper<?> createNodeMapper(TypeInfo typeInfo, NodeMapperSelector selector) {
+        if (typeInfo instanceof ClassInfo classInfo) {
+            if (classInfo.classKind() == ClassKind.ENUM) {
+                return new EnumNodeMapper<>(classInfo);
+            }
+        }
+        return null;
+    }
+
+}
---
+++ src/main/java/cool/scx/object/mapping/mapper_factory/ObjectArrayNodeMapperFactory.java
@@ -0,0 +1,23 @@
+package cool.scx.object.mapping.mapper_factory;
+
+import cool.scx.object.mapping.NodeMapper;
+import cool.scx.object.mapping.NodeMapperFactory;
+import cool.scx.object.mapping.NodeMapperSelector;
+import cool.scx.object.mapping.mapper.ObjectArrayNodeMapper;
+import cool.scx.reflect.ArrayTypeInfo;
+import cool.scx.reflect.TypeInfo;
+
+public class ObjectArrayNodeMapperFactory implements NodeMapperFactory {
+
+    @Override
+    public NodeMapper<?> createNodeMapper(TypeInfo typeInfo, NodeMapperSelector selector) {
+        // 只处理 数组
+        if (typeInfo instanceof ArrayTypeInfo arrayTypeInfo) {
+            var componentType = arrayTypeInfo.componentType();
+            var componentNodeMapper = selector.findNodeMapper(componentType);
+            return new ObjectArrayNodeMapper(arrayTypeInfo, componentNodeMapper);
+        }
+        return null;
+    }
+
+}
