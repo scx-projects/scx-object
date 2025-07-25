7686e3b45848cbfce27f8889e0b999ae2ab03961
scx567888 <scx567888@outlook.com>
2025-07-25 14:34:37 +0800

    dev

--- src/main/java/cool/scx/object/mapping/mapper/time/TemporalAccessorNodeMapper.java
+++ src/main/java/cool/scx/object/mapping/mapper/time/TemporalAccessorNodeMapper.java
@@ -7,22 +7,34 @@ import cool.scx.object.mapping.ToNodeContext;
 import cool.scx.object.node.Node;
 import cool.scx.object.node.TextNode;
 
-import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalQuery;
 
 /// LocalDateTimeNodeMapper
 ///
 /// @author scx567888
 /// @version 0.0.1
-public final class LocalDateTimeNodeMapper implements NodeMapper<LocalDateTime> {
+public final class TemporalAccessorNodeMapper<T extends TemporalAccessor> implements NodeMapper<T> {
 
-    @Override
-    public Node toNode(LocalDateTime value, ToNodeContext context) {
-        return new TextNode(context.options().localDateTimeFormatter().format(value));
+    private final DateTimeFormatter formatter;
+    private final boolean useTimestamp;
+    private final TemporalQuery<T> temporalQuery;
+
+    public TemporalAccessorNodeMapper(DateTimeFormatter formatter, boolean useTimestamp, TemporalQuery<T> temporalQuery) {
+        this.formatter = formatter;
+        this.useTimestamp = useTimestamp;
+        this.temporalQuery = temporalQuery;
     }
 
     @Override
-    public LocalDateTime fromNode(Node node, FromNodeContext context) throws NodeMappingException {
+    public Node toNode(T value, ToNodeContext context) {
+        return new TextNode(formatter.format(value));
+    }
+
+    @Override
+    public T fromNode(Node node, FromNodeContext context) throws NodeMappingException {
         //1, 处理 null
         if (node.isNull()) {
             return null;
@@ -30,7 +42,7 @@ public final class LocalDateTimeNodeMapper implements NodeMapper<LocalDateTime> 
         //2, 只处理 TextNode 类型
         if (node instanceof TextNode textNode) {
             try {
-                return LocalDateTime.parse(textNode.asText(), context.options().localDateTimeFormatter());
+                return formatter.parse(textNode.asText(), temporalQuery);
             } catch (DateTimeParseException e) {
                 throw new NodeMappingException(e);
             }
