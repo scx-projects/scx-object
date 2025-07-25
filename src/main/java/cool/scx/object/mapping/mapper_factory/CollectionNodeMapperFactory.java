b01f97753c47b20d0fc4bcb66e6e3f42c45342f4
scx567888 <scx567888@outlook.com>
2025-07-25 15:44:44 +0800

    dev

---
+++ src/main/java/cool/scx/object/mapping/mapper_factory/CollectionNodeMapperFactory.java
@@ -0,0 +1,24 @@
+package cool.scx.object.mapping.mapper_factory;
+
+import cool.scx.object.mapping.NodeMapper;
+import cool.scx.object.mapping.NodeMapperFactory;
+import cool.scx.object.mapping.NodeMapperSelector;
+import cool.scx.object.mapping.mapper.MapNodeMapper;
+import cool.scx.reflect.ClassInfo;
+import cool.scx.reflect.TypeInfo;
+
+import java.util.Collection;
+
+public class CollectionNodeMapperFactory implements NodeMapperFactory {
+
+    @Override
+    public NodeMapper<?> createNodeMapper(TypeInfo typeInfo, NodeMapperSelector selector) {
+        if (typeInfo instanceof ClassInfo classInfo) {
+            if (Collection.class.isAssignableFrom(typeInfo.rawClass())) {
+                return new MapNodeMapper(classInfo, selector);
+            }
+        }
+        return null;
+    }
+
+}
