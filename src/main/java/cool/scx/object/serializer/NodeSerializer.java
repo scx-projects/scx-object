e65266a56dac84320b9df2a0f7fa69774d1d373f
scx567888 <scx567888@outlook.com>
2025-07-31 16:05:23 +0800

    dev

--- src/main/java/cool/scx/object/serializer/NodeSerializer.java
+++ src/main/java/cool/scx/object/serializer/NodeSerializer.java
@@ -1,9 +1,10 @@
 package cool.scx.object.serializer;
 
+import cool.scx.object.mapping.NodeMappingException;
 import cool.scx.object.node.Node;
 
 public interface NodeSerializer {
 
-    String serializeAsString(Node node);
+    String serializeAsString(Node node) throws NodeSerializeException;
 
 }
