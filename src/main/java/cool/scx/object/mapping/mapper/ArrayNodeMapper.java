f769cb1196cd1dd4e8d591530389c3cfa7b108e1
scx567888 <scx567888@outlook.com>
2025-08-01 12:18:46 +0800

    dev

--- src/main/java/cool/scx/object/mapping/mapper/ArrayNodeMapper.java
+++ src/main/java/cool/scx/object/mapping/mapper/ArrayNodeMapper.java
@@ -10,6 +10,10 @@ import cool.scx.reflect.ArrayTypeInfo;
 
 /// ArrayNodeMapper
 ///
+/// 我们同时支持 基本类型数组和 Object[]
+///
+/// 同时还支持自动将非 ArrayNode 类型包装为 数组类型 以提高兼容性
+///
 /// @author scx567888
 /// @version 0.0.1
 public final class ArrayNodeMapper implements NodeMapper<Object> {
