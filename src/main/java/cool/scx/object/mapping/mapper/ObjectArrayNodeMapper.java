f40ad3797cbf3ef0775d3f3ecfa9e3499a6a2b67
scx567888 <scx567888@outlook.com>
2025-08-01 11:59:54 +0800

    dev

--- src/main/java/cool/scx/object/mapping/mapper/ObjectArrayNodeMapper.java
+++ src/main/java/cool/scx/object/mapping/mapper/ObjectArrayNodeMapper.java
@@ -12,7 +12,7 @@ import cool.scx.reflect.ArrayTypeInfo;
 ///
 /// @author scx567888
 /// @version 0.0.1
-public final class ObjectArrayNodeMapper implements NodeMapper<Object[]> {
+public final class ObjectArrayNodeMapper implements NodeMapper<Object> {
 
     private final ArrayTypeInfo arrayTypeInfo;
     private final NodeMapper<Object> componentNodeMapper;
@@ -24,14 +24,80 @@ public final class ObjectArrayNodeMapper implements NodeMapper<Object[]> {
     }
 
     @Override
-    public Node toNode(Object[] value, ToNodeContext context) throws NodeMappingException {
-        var arrayNode = new ArrayNode(value.length);
-        var i = 0;
-        for (var a : value) {
-            arrayNode.add(context.toNode(a, i));
-            i = i + 1;
+    public Node toNode(Object value, ToNodeContext context) throws NodeMappingException {
+        switch (value) {
+            // 处理 Object[]
+            case Object[] arr -> {
+                var arrayNode = new ArrayNode(arr.length);
+                var i = 0;
+                for (var a : arr) {
+                    // 我们不能确定 每个数组元素一定是 componentNodeMapper 支持的类型
+                    // 所以这里 需要 递归调用 context.toNode
+                    // 但是对于基本类型则不需要
+                    arrayNode.add(context.toNode(a, i));
+                    i = i + 1;
+                }
+                return arrayNode;
+            }
+            case byte[] arr -> {
+                var arrayNode = new ArrayNode(arr.length);
+                for (var a : arr) {
+                    arrayNode.add(componentNodeMapper.toNode(a, context));
+                }
+                return arrayNode;
+            }
+            case short[] arr -> {
+                var arrayNode = new ArrayNode(arr.length);
+                for (var a : arr) {
+                    arrayNode.add(componentNodeMapper.toNode(a, context));
+                }
+                return arrayNode;
+            }
+            case int[] arr -> {
+                var arrayNode = new ArrayNode(arr.length);
+                for (var a : arr) {
+                    arrayNode.add(componentNodeMapper.toNode(a, context));
+                }
+                return arrayNode;
+            }
+            case long[] arr -> {
+                var arrayNode = new ArrayNode(arr.length);
+                for (var a : arr) {
+                    arrayNode.add(componentNodeMapper.toNode(a, context));
+                }
+                return arrayNode;
+            }
+            case float[] arr -> {
+                var arrayNode = new ArrayNode(arr.length);
+                for (var a : arr) {
+                    arrayNode.add(componentNodeMapper.toNode(a, context));
+                }
+                return arrayNode;
+            }
+            case double[] arr -> {
+                var arrayNode = new ArrayNode(arr.length);
+                for (var a : arr) {
+                    arrayNode.add(componentNodeMapper.toNode(a, context));
+                }
+                return arrayNode;
+            }
+            case boolean[] arr -> {
+                var arrayNode = new ArrayNode(arr.length);
+                for (var a : arr) {
+                    arrayNode.add(componentNodeMapper.toNode(a, context));
+                }
+                return arrayNode;
+            }
+            case char[] arr -> {
+                var arrayNode = new ArrayNode(arr.length);
+                for (var a : arr) {
+                    arrayNode.add(componentNodeMapper.toNode(a, context));
+                }
+                return arrayNode;
+            }
+            // 不是数组类型, 这里基本上是不可达的, 如果不是外部直接调用的话
+            default -> throw new NodeMappingException("Unsupported type: " + value.getClass());
         }
-        return arrayNode;
     }
 
     @Override
