0eaa75e922579d146f0d16388a78143ac3f82148
scx567888 <scx567888@outlook.com>
2025-09-30 12:20:31 +0800

    init

---
+++ src/test/java/cool/scx/object/test/ConvertValueTest.java
@@ -0,0 +1,40 @@
+package cool.scx.object.test;
+
+import cool.scx.object.ScxObject;
+import cool.scx.reflect.TypeReference;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class ConvertValueTest {
+
+    public static void main(String[] args) {
+        test1();
+    }
+
+    @Test
+    public static void test1() {
+
+        var a = ScxObject.convertValue("123", int.class);
+        var b = ScxObject.convertValue("123", int[].class);
+        var c = ScxObject.convertValue("123", int[][].class);
+        var d = ScxObject.convertValue("123", List.class);
+        var e = ScxObject.convertValue("123", new TypeReference<List<List<Integer>>>() {});
+
+        var a1 = ScxObject.convertValue(new Object[]{"123"}, int.class);
+        var b1 = ScxObject.convertValue(new Object[]{"123"}, int[].class);
+        var c1 = ScxObject.convertValue(new Object[]{"123"}, int[][].class);
+        var d1 = ScxObject.convertValue(new Object[]{"123"}, List.class);
+        var e1 = ScxObject.convertValue(new Object[]{"123"}, new TypeReference<List<List<Integer>>>() {});
+
+
+        var a2 = ScxObject.convertValue(new Object[][]{new Object[]{"123"}}, int.class);
+        var b2 = ScxObject.convertValue(new Object[][]{new Object[]{"123"}}, int[].class);
+        var c2 = ScxObject.convertValue(new Object[][]{new Object[]{"123"}}, int[][].class);
+        var d2 = ScxObject.convertValue(new Object[][]{new Object[]{"123"}}, List.class);
+        var e2 = ScxObject.convertValue(new Object[][]{new Object[]{"123"}}, new TypeReference<List<List<Integer>>>() {});
+        System.out.println();
+    }
+
+
+}
---
+++ src/test/java/cool/scx/object/test/NodeHelperTest.java
@@ -0,0 +1,23 @@
+package cool.scx.object.test;
+
+import cool.scx.object.NodeHelper;
+import cool.scx.object.mapping.NodeMappingException;
+import cool.scx.object.node.ObjectNode;
+import org.testng.annotations.Test;
+
+public class NodeHelperTest {
+
+    public static void main(String[] args) throws NodeMappingException {
+        test1();
+    }
+
+    @Test
+    public static void test1() throws NodeMappingException {
+        var objectNode = new ObjectNode();
+        NodeHelper.set(objectNode, "a.b.c.d.e.f", new int[]{1, 2, 3, 4});
+        System.out.println(objectNode);
+        System.out.println(NodeHelper.get(objectNode, "a.b.c"));
+        System.out.println(NodeHelper.get(objectNode, "a.b.c.d.e.f"));
+    }
+
+}
