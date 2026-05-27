32e80fc0a8f027adcb5e6907d348a2abe508bbce
scx567888 <scx567888@outlook.com>
2026-05-27 15:51:44 +0800

    init

---
+++ src/main/java/dev/scx/object/ObjectNodeConvertOptions.java
@@ -0,0 +1,9 @@
+package dev.scx.object;
+
+/// ObjectNodeConvertOptions
+///
+/// @author scx567888
+/// @version 0.0.1
+public interface ObjectNodeConvertOptions {
+
+}
---
+++ src/main/java/dev/scx/object/ObjectToNodeException.java
@@ -0,0 +1,21 @@
+package dev.scx.object;
+
+/// ObjectToNodeException
+///
+/// @author scx567888
+/// @version 0.0.1
+public class ObjectToNodeException extends RuntimeException {
+
+    public ObjectToNodeException(String message) {
+        super(message);
+    }
+
+    public ObjectToNodeException(Throwable cause) {
+        super(cause);
+    }
+
+    public ObjectToNodeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
